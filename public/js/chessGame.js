740c99a87f5771afa9136223430fc09650612f73
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-02 13:56:45 +0530

    updated chessGame.js and index.ejs

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -1,2 +1,25 @@
 const socket=io();
 socket.emit("hello");
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null; 
+let playerRole = null;
+
+const renderBoard = () => {
+     const board = chess.board();
+     boardElement.innerHTML="";
+     board.forEach((row,rowindex)=>{
+        row.forEach((square,squareindex)=>{
+            console.log(square);
+        })
+     })
+}
+const handleMove = () => {
+
+}
+const getPieceUnicode = () => {
+
+}
+renderBoard();
