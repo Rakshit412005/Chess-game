91a2056b963a16ccd7944f0e7b2d53a2681c4b77
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-02 14:47:02 +0530

    added css

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -13,6 +13,27 @@ const renderBoard = () => {
      board.forEach((row,rowindex)=>{
         row.forEach((square,squareindex)=>{
             console.log(square);
+            const squareElement = document.createElement("div"); // boxes ke design and color ke liye logic
+            squareElement.classList.add(
+                "square",
+                (rowindex + squareindex)% 2 === 0 ? "light" : "dark"
+            )
+            if (square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add(
+                    "piece",
+                    square.color === "w" ? "white" : "black"
+                );
+                pieceElement.innerText=""
+                pieceElement.draggable = playerRole === square.color;
+                pieceElement.addEventListener("dragstart",()=>{
+                    if(pieceElement.draggable){
+                        draggedPiece=pieceElement;
+                        sourceSquare = {row: rowindex, col: squareindex};
+                    }
+
+                })
+            }
         })
      })
 }
